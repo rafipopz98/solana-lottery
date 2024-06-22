19b09ec7be779226d6a7b1888d148619b025ed4e
rafipopz98 <itsrealrafi@gmail.com>
2024-06-23 02:01:27 +0530

    first commit

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,14 +1,29 @@
+import { ConnectionProvider } from "@solana/wallet-adapter-react";
 import Header from "../components/Header";
 import PotCard from "../components/PotCard";
 import Table from "../components/Table";
 import style from "../styles/Home.module.css";
-
+import { useMemo } from "react";
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { WalletProvider } from "@solana/wallet-adapter-react";
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
+require("@solana/wallet-adapter-react-ui/styles.css")
 export default function Home() {
+  const endpoint = "https://orbital-spring-sun.solana-devnet.quiknode.pro/5844d2d814beb31ff2132bc56864920050a5931c/";
+  const wallets = useMemo(() => [
+    new PhantomWalletAdapter(),
+  ], [])
   return (
-    <div className={style.wrapper}>
-      <Header />
-      <PotCard />
-      <Table />
-    </div>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>
+          <div className={style.wrapper}>
+            <Header />
+            <PotCard />
+            <Table />
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider >
   );
 }
